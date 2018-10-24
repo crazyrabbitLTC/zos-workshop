34e0ce7274bc75e07c6555969c284ba5ca3a142f
Santiago Palladino <spalladino@gmail.com>
2018-10-24 15:30:52 +0200

    Fix OwnableWallet solution

--- contracts/solutions/OwnableWallet.sol
+++ contracts/solutions/OwnableWallet.sol
@@ -1,21 +1,11 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-eth/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-eth/contracts/ownership/Ownable.sol";
 import "zos-lib/contracts/Initializable.sol";
 
-contract SecureWallet is Initializable {
-  address _owner;
-  
-  function initialize(address owner) initializer public {
-    _owner = owner;
-  }
-
-  function owner() public view returns (address) {
-    return _owner;
-  }
-
-  function transferERC20(IERC20 token, address to, uint256 amount) public returns (bool) {
-    require(msg.sender == _owner);
+contract OwnableWallet is Ownable {
+  function transferERC20(IERC20 token, address to, uint256 amount) public onlyOwner returns (bool) {
     require(token.transfer(to, amount));
   }
 }
